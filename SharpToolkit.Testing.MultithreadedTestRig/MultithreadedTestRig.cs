c4ae01e1f7470d4abf45578b869ee793981c5383
dmitriys <dmitriys@codevalue.net>
2017-11-30 14:23:56 +0200

    Moved from SharpToolkit.Extensions

---
+++ SharpToolkit.Testing.MultithreadedTestRig/MultithreadedTestRig.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SharpToolkit.Testing
+{
+    public abstract class MultithreadedTestRig
+    {
+        public static Action OnContractFailedAction { get; set; }
+
+        public class ContractFailedOnTestException : Exception
+        {
+            public string Condition { get; }
+
+            public ContractFailedOnTestException(string message, string condition, Exception originalException) : base(message, originalException)
+            {
+                this.Condition = condition;
+            }
+        }
+
+        static MultithreadedTestRig()
+        {
+            Contract.ContractFailed += (sender, args) =>
+            {
+                // TODO: Pass the contract arguments inside the action.
+                OnContractFailedAction?.Invoke();
+
+                throw new ContractFailedOnTestException(args.Message, args.Condition, args.OriginalException);
+            };
+        }
+    }
+
+    public class MultithreadedTestRig<TThreadContext> : MultithreadedTestRig
+    {
+        private AutoResetEvent[] mainContinueEvent;
+
+        private object syncRoot =
+            new object();
+
+        private List<(Thread, TThreadContext, Action<TThreadContext>)> threads;
+        public IReadOnlyList<(Thread, TThreadContext, Action<TThreadContext>)> Threads => threads;
+
+        public bool Success { get; private set; } = true;
+        public Exception CaughtException { get; private set; }
+        public string CaughtExceptionThread { get; private set; }
+
+        public MultithreadedTestRig()
+        {
+            this.threads = new List<(Thread, TThreadContext, Action<TThreadContext>)>();
+        }
+
+        public void CreateThread(Action<TThreadContext> runAct, Action<TThreadContext> abortAct, TThreadContext context)
+        {
+            CreateThread(runAct, abortAct, context, "TestThread");
+        }
+
+        public void CreateThread(Action<TThreadContext> runAct, Action<TThreadContext> abortAct, TThreadContext context, string name)
+        {
+            var t = new Thread((object i) =>
+            {
+                try
+                {
+                    runAct(context);
+                }
+                catch (Exception e)
+                {
+                    lock (this.syncRoot)
+                    {
+                        if (this.CaughtException == null)
+                        {
+                            this.CaughtException = e;
+                            this.CaughtExceptionThread = Thread.CurrentThread.Name;
+                            this.Success = false;
+
+                            AbortThreads();
+                        }
+                    }
+                }
+                finally
+                {
+                    this.mainContinueEvent[(int)i].Set();
+                }
+            })
+            {
+                Name = name
+            };
+
+            this.threads.Add((t, context, abortAct));
+        }
+
+        public void AbortThreads()
+        {
+            foreach (var (thread, ctx, abortAct) in this.Threads)
+            {
+                if (thread != Thread.CurrentThread)
+                    abortAct(ctx);
+            }
+        }
+
+        public void StartThreads()
+        {
+            this.mainContinueEvent =
+                this.Threads.Select(t => new AutoResetEvent(false)).ToArray();
+
+            for (int i = 0; i < this.Threads.Count; i += 1)
+                this.Threads[i].Item1.Start(i);
+        }
+
+        public void WaitForThreads()
+        {
+            WaitHandle.WaitAll(this.mainContinueEvent);
+        }
+    }
+}
---
+++ SharpToolkit.Testing.MultithreadedTestRig/README.md
@@ -0,0 +1,43 @@
+﻿# Multithreaded Test Rig
+
+Allow to easily define the conditions for tests that require concurrency. It helps to coupe with test runner not handling assertions, exceptions and contracts outside the main thread.
+
+The usage is pretty simple:
+
+```CSharp
+// The TestContext is the type of class that would be used to
+// interact with the threads.
+var rig = new MultithreadedTestRig<TestContext>();
+
+// Any number of threads can be created.
+rig.CreateThread(
+    // The code that the thread will run.
+    ctx =>
+    {
+        while (true)
+        {
+            Thread.Sleep(100);
+            if (ctx.CancellationRequested)
+                break;
+        }
+    },
+    // The code that will execute when the rig will ask
+    // to abort the execution.
+    ctx =>
+    {
+        ctx.CancellationRequested = true;
+    },
+    // The context
+    new TestContext(),
+    // Optional thread name.
+    "Thread name"
+);
+
+rig.StartThreads();
+rig.WaitForThreads();
+
+// Success will false if an exception is thrown
+// a Contract has failed.
+rig.Success.Should().BeTrue();
+rig.CaughtException.Should().BeNull();
+```
