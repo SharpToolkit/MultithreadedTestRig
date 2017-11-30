9bf289c5c61e457f1c409a910690322dde51e71e
dmitriys <dmitriys@codevalue.net>
2017-11-30 15:06:17 +0200

    Build.bat for MyGet

---
+++ build.bat
@@ -0,0 +1,43 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+
+set version=
+if not "%PackageVersion%" == "" (
+   set version=-Version %PackageVersion%
+)
+
+REM Detect MSBuild 15.0 path
+if exist "%programfiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" (
+    set msbuild="%programfiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+)
+if exist "%programfiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" (
+    set msbuild="%programfiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
+)
+if exist "%programfiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" (
+    set msbuild="%programfiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
+)
+
+REM restore
+call dotnet restore
+if not "%errorlevel%"=="0" goto failure
+
+REM build
+call "%msbuild%" SharpToolkit.Testing.MultithreadedTestRig.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+if not "%errorlevel%"=="0" goto failure
+
+call dotnet test SharpToolkit.Testing.MultithreadedTestRig.Tests\SharpToolkit.Testing.MultithreadedTestRig.Tests.csproj --configuration %config% --no-build
+if not "%errorlevel%"=="0" goto failure
+
+REM Package
+mkdir %cd%\..\artifacts
+call dotnet pack SharpToolkit.Testing.MultithreadedTestRig --configuration %config% %packversionsuffix% --output %cd%\..\artifacts
+if not "%errorlevel%"=="0" goto failure
+
+:success
+exit 0
+
+:failure
+exit -1
